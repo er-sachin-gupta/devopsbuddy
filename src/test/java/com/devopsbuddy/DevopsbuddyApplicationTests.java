2dadc4844cc3c91836870241a81e2ed42f3496ab
Sachin Gupta <sgs.engineer@yahoo.co.in>
2022-10-21 06:19:03 +0530

    I18N Capabilties

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,6 +1,9 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18NService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +11,17 @@ class DevopsbuddyApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private I18NService i18NService;
+
+    @Test
+    public void testMessageByLocaleService() throws Exception {
+        String expectedResult = "Bootstrap starter template";
+        String messageId = "index.main.callout";
+        String actual = i18NService.getMessage(messageId);
+        Assertions.assertEquals(expectedResult, actual, "The actual and expected string don't match");
     }
 
 }
